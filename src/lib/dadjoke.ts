1f98da505a9724b7b29318e3ac65cee85e949d9d
Matt Bogenhagen <i3oges@gmail.com>
2022-07-09 10:12:41 -0400

    make the joke in gigispeak

--- src/lib/dadjoke.ts
+++ src/lib/dadjoke.ts
@@ -1,11 +1,11 @@
-import { findAllTextChannels } from './utils';
+import { findAllTextChannels, gigiSpeak } from './utils';
 import request from 'axios';
 import { Client } from 'discord.js';
 export const postDadJoke = async (client: Client) => {
   const channels = findAllTextChannels(client, 'general');
   const { data } = await request('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
   for (const channel of channels) {
-    await channel.send(data.joke);
+    await channel.send(gigiSpeak(data.joke));
     console.log('sent dad joke at', new Date().toString(), 'to', channel.name);
   }
   console.log('sent dad joke at', new Date().toString(), 'to', channels.length, 'channels');
