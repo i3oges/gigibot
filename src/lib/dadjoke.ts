b42bd008f39588af0f336e5af3af043dacefc712
Matt Bogenhagen <i3oges@gmail.com>
2022-07-12 07:49:59 -0400

    remove axios and use experimental node fetch

--- src/lib/dadjoke.ts
+++ src/lib/dadjoke.ts
@@ -1,12 +1,17 @@
-import { findAllTextChannels, gigiSpeak } from './utils';
-import request from 'axios';
 import { Client } from 'discord.js';
+import { findAllTextChannels, gigiSpeak } from './utils';
+
 export const postDadJoke = async (client: Client) => {
   const channels = findAllTextChannels(client, 'general');
-  const { data } = await request('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
-  for (const channel of channels) {
-    await channel.send(gigiSpeak(data.joke));
-    console.log('sent dad joke at', new Date().toString(), 'to', channel.name);
+  const data = await fetch('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } }).then(res => res.json());
+  const { joke } = data;
+  try {
+    for (const channel of channels) {
+      await channel.send(gigiSpeak(joke));
+      console.log('sent dad joke at', new Date().toString(), 'to', channel.name);
+    }
+  } catch (err) {
+    console.error('error posting joke', err);
   }
   console.log('sent dad joke at', new Date().toString(), 'to', channels.length, 'channels');
 };
--- src/lib/inspiration.ts
+++ src/lib/inspiration.ts
@@ -1,10 +1,9 @@
-import request from 'axios';
 import { Client } from 'discord.js';
 import { findAllTextChannels } from './utils';
 export const postInspiration = async (client: Client) => {
   const channels = findAllTextChannels(client, 'inspiration');
   // fetch from inspirobot
-  const { data } = await request('https://inspirobot.me/api?generate=true');
+  const { data } = await fetch('https://inspirobot.me/api?generate=true').then(res => res.json());
   for (const channel of channels) {
     await channel.send(data);
     console.log('sent inspiration at', new Date().toString(), 'to', channel.name);
